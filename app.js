0ae78d78d04c1920124578c2a9ea651571a6e19e
MikolajUz <muzialko@gmail.com>
2024-09-04 16:39:31 +0200

    html+js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-// Import the http module
-const http = require("http");
+// // Import the http module
+// const http = require("http");
 
-// Create a server that sends a "Hello World" response
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; // Set status code to 200 (OK)
-  res.setHeader("Content-Type", "text/plain"); // Set response type
-  res.end("Hello World\n"); // Send response
-});
+// // Create a server that sends a "Hello World" response
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200; // Set status code to 200 (OK)
+//   res.setHeader("Content-Type", "text/plain"); // Set response type
+//   res.end("Hello World\n"); // Send response
+// });
 
-// Define the port to listen on
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+// // Define the port to listen on
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
+
